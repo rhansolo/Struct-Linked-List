51a1f2a1e601500483654a5d759a4e342245680f
robinhanstuy <33820379+robinhanstuy@users.noreply.github.com>
2018-10-11 03:20:33 -0400

    forgot to update

--- test.c
+++ test.c
@@ -1,2 +1,16 @@
-#include "structlink.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include "structlink.h"
 
+int main(){
+	struct node *list = malloc(sizeof(struct node));
+	list -> i = 2;
+	list -> next = NULL;
+	list = insert_front(list,-3);
+	print_list(list);
+	list = insert_front(list,2);
+	print_list(list);
+	list = free_list(list);
+	printf("after free the list is now %s\n",(char*)list);
+	return 0;
+}
